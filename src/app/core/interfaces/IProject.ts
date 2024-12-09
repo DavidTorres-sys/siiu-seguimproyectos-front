d3ce32cd07da7e773880d6b7bdf0d2969ea4f9c8
DavidTorres-sys <david.torresg@udea.edu.co>
2024-12-09 15:26:41 -0500

    add sonar project configuration and refactor interfaces to use 'I' prefix; implement project and type services with API integration

--- src/app/core/interfaces/IProject.ts
+++ src/app/core/interfaces/IProject.ts
@@ -1,4 +1,4 @@
-export interface Project {
+export interface IProject {
   code: string;
   projectName: string;
   level: string;
--- src/app/shared/services/type/type.service.ts
+++ src/app/shared/services/type/type.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { IType } from 'src/app/core/interfaces/IType';
 
+/**
+ * Service class for managing operations related to types.
+ * 
+ * This service provides methods to interact with the backend for fetching
+ * and managing type-related data. It is provided at the root level of the Angular
+ * application, making it accessible across all components and modules.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +18,23 @@ export class TypeService {
 
   private url = `${environment.route}tipos`;
 
+  /**
+   * Constructor to inject the HttpClient dependency for performing HTTP requests.
+   * 
+   * @param http - An Angular service to make HTTP requests.
+   */
   constructor(private http: HttpClient) { }
 
-  getTypes(pageSize: number, pageNumber: number): Observable<any> {
+  /**
+   * Retrieves a paginated list of types from the API.
+   * 
+   * This method makes a GET request to the API with pagination parameters and custom headers.
+   * 
+   * @param pageSize - The number of types to retrieve per page.
+   * @param pageNumber - The page number to fetch.
+   * @returns An Observable that emits an array of `IType` objects.
+   */
+  getAll(pageSize: number, pageNumber: number): Observable<IType[]> {
     const headers = {
       'message-uuid': '<uuid>',
       'request-app-id': '<uuid>',
@@ -22,6 +44,7 @@ export class TypeService {
       pageSize: pageSize.toString(),
       pageNumber: pageNumber.toString()
     };
-    return this.http.get(this.url, { headers: headers, params: params });
+
+    return this.http.get<IType[]>(this.url, { headers: headers, params: params });
   }
 }
