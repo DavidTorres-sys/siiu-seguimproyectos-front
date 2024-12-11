e7733464c5b95eebaf17c300229ab6988943ca45
DavidTorres-sys <david.torresg@udea.edu.co>
2024-12-11 08:48:27 -0500

    refactor alert component: remove obsolete HTML, implement inline template with Material Design, and add alert functionality to sidebar

--- src/app/shared/components/atoms/alert/alert.component.ts
+++ src/app/shared/components/atoms/alert/alert.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
-  templateUrl: './alert.component.html',
+  template: `
+  <mat-card>
+    <mat-icon class="alert-icon" *ngIf="icon">{{ icon }}</mat-icon>
+    <span class="alert-label">{{ label }}</span>
+  </mat-card>
+  `,
   styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent {
-
+  @Input() label!: string;
+  @Input() icon: string = '';
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import {MatIconModule} from '@angular/material/icon';;
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';;
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NgFor } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AlertComponent } from './components/atoms/alert/alert.component';
 import { InputTextComponent } from './components/atoms/input-text/input-text.component';
 import { ButtonSecondaryComponent } from './components/atoms/button-secondary/button-secondary.component';
@@ -12,14 +18,6 @@ import { SidebarContentComponent } from './components/molecules/sidebar-content/
 import { SidebarComponent } from './components/organisms/sidebar/sidebar.component';
 import { DropdownComponent } from './components/atoms/dropdown/dropdown.component';
 import { DropdownRequiredComponent } from './components/atoms/dropdown-required/dropdown-required.component';
-
-
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {NgFor} from '@angular/common';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
 @NgModule({
   declarations: [
     AlertComponent,
@@ -43,6 +41,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     FormsModule,
     MatSidenavModule, 
     NgIf,
+    MatCardModule
   ],
   exports: [
     MatIconModule,
